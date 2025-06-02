73333912bc2f578ebe289f9536dec885ae2e1286
Jack Duignan <jackpduignan@gmail.com>
2025-06-02 14:53:22 +1200

    playback working

--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,43 @@
 use num_complex::Complex;
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 
-pub fn save_data(data: &[Complex<i16>]) {
+pub fn save_data(data: &[Complex<i16>]) -> Result<(), Box<dyn std::error::Error>> {
     let now = std::time::SystemTime::now()
         .duration_since(std::time::SystemTime::UNIX_EPOCH)
         .unwrap()
         .as_millis();
     let name = format!("test_{now}.iq");
-    let mut file = std::fs::File::create(name).unwrap();
+    let mut file = std::fs::File::create(name)?;
 
     for d in data {
         file.write_i16::<LittleEndian>(d.im).unwrap();
         file.write_i16::<LittleEndian>(d.re).unwrap();
     }
+
+    Ok(())
+}
+
+pub fn load_data(filename: String) -> Result<Vec<Complex<i16>>, Box<dyn std::error::Error>> {
+    let mut file = std::fs::File::open(filename)?;
+    let mut r_buf: Vec<Complex<i16>> = Vec::new();
+
+    loop {
+        let im = match file.read_i16::<LittleEndian>() {
+            Ok(val) => val,
+            Err(ref e) if e.kind() == std::io::ErrorKind::UnexpectedEof => break,
+            Err(e) => return Err(Box::new(e)),
+        };
+
+        let re = match file.read_i16::<LittleEndian>() {
+            Ok(val) => val,
+            Err(ref e) if e.kind() == std::io::ErrorKind::UnexpectedEof => break,
+            Err(e) => return Err(Box::new(e)),
+        };
+
+        r_buf.push(Complex::new(re, im));
+    }
+
+    Ok(r_buf)
 }
 
 /// Calculate the magnitude of every complex pair in the buffer
